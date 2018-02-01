a2191b7217adc5cf96150794600cf760eb9d9820
MirroredFate <34125179+MirroredFate@users.noreply.github.com>
2018-02-01 09:56:24 +0100

    1
    1

---
+++ Assets/Scripts/Background.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Background : MonoBehaviour {
+
+    public GameObject Back;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    void OnTriggerExit(Collider collision)
+    {
+        if (collision.tag == "Back")
+        {
+
+            Debug.LogFormat("1");
+        }
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Back")
+        {
+            Destroy(gameObject);
+            Destroy(collision.gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+    }
+}
