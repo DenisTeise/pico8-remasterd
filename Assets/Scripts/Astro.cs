708f249ebea0030b32434acd2bd250ee5f9eefa8
MrBasenji <34299221+MrBasenji@users.noreply.github.com>
2018-02-01 10:36:08 +0100

    spawn
    enemies

--- Assets/Scripts/Astro.cs
+++ Assets/Scripts/Astro.cs
@@ -4,21 +4,44 @@ using UnityEngine;
 
 public class Astro : MonoBehaviour {
 
+    public GameObject asteroidPrefab;
+    GameObject mySpaceship;
+    Vector3 myVector3;
+    public int range;
+    public int counter;
+
 	// Use this for initialization
 	void Start () {
-		
+        counter = 0;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	 
-	}
+        if (Input.GetMouseButtonDown(1))
+        {
+            spawnEnemies();
+        }
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.tag=="Shot")
         {
             Destroy(gameObject);
             Destroy(collision.gameObject);
+        }
+    }
+
+    void spawnEnemies()
+    {
+        mySpaceship = GameObject.Find("idle_player_basic");
+        if (mySpaceship != null)
+        {
+            for (int i = 0; i < counter; i++)
+            {
+                myVector3 = Random.insideUnitCircle * range;
+                Instantiate(asteroidPrefab, myVector3, Quaternion.identity);
+            }
         }
     }
 }
